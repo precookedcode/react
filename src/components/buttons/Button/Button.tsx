a9cebe7f3535825a98a057d932133ddc35a5817c
Roberto Carraro <roberto.carraro@outlook.com>
2025-01-07 23:51:42 -0300

    feat(Button): add TypingEffect

--- src/components/buttons/Button/Button.tsx
+++ src/components/buttons/Button/Button.tsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
-import Icon from "../../Icon/Icon";
+import React, { useState, forwardRef } from "react";
 import DynamicIcon from "../../DynamicIcon/DynamicIcon";
+import Icon from "../../Icon/Icon";
 import { colors } from "../../../config";
 import Touchable from "../../Touchable/Touchable";
+import TypingEffect from "../../TypingEffect/TypingEffect";
+
+
 
 const hexToRgb = (hex: string) => {
     const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
@@ -22,7 +25,6 @@ const isDarkColor = (color: string) => {
     if (!rgb) return false;
 
     const { r, g, b } = rgb;
-    // Formula for calculating brightness
     const brightness = (r * 299 + g * 587 + b * 114) / 1000;
     return brightness < 128;
 };
@@ -31,12 +33,24 @@ const resolveIcon = (
     icon: string | undefined,
     paths?: any[],
     size?: number,
-    color?: string
+    color?: string,
+    style?: React.CSSProperties
 ) => {
+
     if (icon) {
-        return <Icon name={icon} size={size} color={color} />;
+        return <div style={{ flex: 1 }}>
+            <Icon name={icon} size={size} color={color} style={{
+                position: 'relative',
+                top: 1,
+                ...style
+            }} />
+        </div>;
     } else if (paths) {
-        return <DynamicIcon paths={paths} size={size} />;
+        return <div style={{ flex: 1 }}><DynamicIcon paths={paths} size={size} style={{
+            position: 'relative',
+            top: 1,
+            ...style
+        }} /></div>;
     }
     return null;
 };
@@ -51,136 +65,177 @@ interface ButtonProps {
     startIcon?: string;
     startIconPaths?: string[];
     startIconSize?: number;
+    startIconStyle?: React.CSSProperties;
+    startIconColor?: string;
     endIcon?: string;
     endIconPaths?: string[];
     endIconSize?: number;
+    endIconStyle?: React.CSSProperties;
+    endIconColor?: string;
     hasShadow?: boolean;
     style?: React.CSSProperties;
     titleStyle?: React.CSSProperties;
     size?: "xs" | "sm" | "md" | "lg" | "xl";
     className?: string;
+    animateTitle?: boolean;
+    animationSpeed?: number; // Nueva prop
+    onAnimationComplete?: () => void; // Nueva prop
 }
 
 const sizeStyles = {
     xs: { padding: "4px", fontSize: "10px", iconSize: 12 },
     sm: { padding: "6px", fontSize: "12px", iconSize: 16 },
-    md: { padding: "10px", fontSize: "16px", iconSize: 24 }, // Default size
+    md: { padding: "10px", fontSize: "16px", iconSize: 24 },
     lg: { padding: "14px", fontSize: "18px", iconSize: 32 },
     xl: { padding: "18px", fontSize: "22px", iconSize: 40 },
 };
 
-const Button: React.FC<ButtonProps> = ({
-    title,
-    onClick,
-    color = "primary",
-    borderRadius = 4,
-    type = "solid",
-    disabled = false,
-    startIcon,
-    startIconPaths,
-    startIconSize,
-    endIcon,
-    endIconPaths,
-    endIconSize,
-    hasShadow = true,
-    style,
-    titleStyle,
-    size = "md",
-    className = "button",
-}) => {
-    const [isPressed, setIsPressed] = useState(false);
+const Button = forwardRef<HTMLDivElement, ButtonProps>(
+    ({
+        title,
+        onClick,
+        color = "primary",
+        borderRadius = 4,
+        type = "solid",
+        disabled = false,
+        startIcon,
+        startIconPaths,
+        startIconSize,
+        startIconStyle,
+        startIconColor,
+        endIcon,
+        endIconPaths,
+        endIconSize,
+        endIconStyle,
+        endIconColor,
+        hasShadow = true,
+        style,
+        titleStyle,
+        size = "md",
+        className = "button",
+        animateTitle = false,
+        animationSpeed = 50, // Velocidad predeterminada
+        onAnimationComplete, // Callback opcional
+    }, ref) => {
+        const [isPressed, setIsPressed] = useState(false);
+        const [showEndIcon, setShowEndIcon] = useState(!animateTitle);
 
-    const handlePressStart = () => {
-        if (!disabled) {
-            setIsPressed(true);
-        }
-    };
+        const handlePressStart = () => {
+            if (!disabled) {
+                setIsPressed(true);
+            }
+        };
 
-    const handlePressEnd = () => {
-        if (!disabled) {
-            setIsPressed(false);
-        }
-    };
+        const handlePressEnd = () => {
+            if (!disabled) {
+                setIsPressed(false);
+            }
+        };
 
-    const iconSize = sizeStyles[size]?.iconSize;
+        const handleAnimationComplete = () => {
+            setShowEndIcon(true); // Mostrar el ícono derecho al completar la animación
+            if (onAnimationComplete) {
+                onAnimationComplete(); // Disparar el callback
+            }
+        };
 
-    const resolvedColor = color in colors ? colors[color as keyof typeof colors] : color;
+        const iconSize = sizeStyles[size]?.iconSize;
 
-    const contentColor =
-        type === "solid"
-            ? isDarkColor(resolvedColor || "")
-                ? "#fff"
-                : colors.textShade
-            : resolvedColor;
+        const resolvedColor = color in colors ? colors[color as keyof typeof colors] : color;
 
-    const buttonStyle: React.CSSProperties = {
-        display: "inline-flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "row",
-        padding: sizeStyles[size]?.padding,
-        borderRadius: `${borderRadius}px`,
-        border: type === "outline" ? `2px solid ${resolvedColor}` : "none",
-        backgroundColor: type === "solid" ? resolvedColor : "transparent",
-        color: contentColor,
-        boxShadow: hasShadow
-            ? isPressed
-                ? "none"
-                : "0px 2px 5px rgba(0, 0, 0, 0.2)"
-            : "none",
-        transform: isPressed ? "scale(0.98)" : "scale(1)",
-        cursor: disabled ? "not-allowed" : "pointer",
-        transition: "all 0.2s ease-in-out",
-        opacity: disabled ? 0.6 : 1,
-        boxSizing: "border-box",
-        ...style,
-    };
+        const contentColor =
+            type === "solid"
+                ? isDarkColor(resolvedColor || "")
+                    ? "#fff"
+                    : colors.textShade
+                : resolvedColor;
 
-    const textStyle: React.CSSProperties = {
-        fontSize: sizeStyles[size]?.fontSize,
-        margin: "0 8px",
-        color: contentColor,
-        ...titleStyle,
-    };
+        const buttonStyle: React.CSSProperties = {
+            display: "inline-flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "row",
+            padding: sizeStyles[size]?.padding,
+            borderRadius: `${borderRadius}px`,
+            border: type === "outline" ? `2px solid ${resolvedColor}` : "none",
+            backgroundColor: type === "solid" ? resolvedColor : "transparent",
+            color: contentColor,
+            boxShadow: hasShadow
+                ? isPressed
+                    ? "none"
+                    : "0px 2px 0px rgba(0, 0, 0, 0.15)"
+                : "none",
+            transform: isPressed ? "scale(0.98)" : "scale(1)",
+            cursor: disabled ? "not-allowed" : "pointer",
+            transition: "all 0.2s ease-in-out",
+            opacity: disabled ? 0.6 : 1,
+            boxSizing: "border-box",
 
-    return (
-        <Touchable className={className} onClick={onClick} style={buttonStyle}>
-            <button
-                onClick={(e) => e.preventDefault()}
-                onMouseDown={handlePressStart} // Desktop
-                onMouseUp={handlePressEnd} // Desktop
-                onMouseLeave={handlePressEnd} // Desktop
-                onTouchStart={handlePressStart} // Mobil
-                onTouchEnd={handlePressEnd} // Mobil
-                style={{
-                    border: "none",
-                    backgroundColor: "transparent",
-                    width: "100%",
-                    height: "100%",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    cursor: "inherit",  // Inherit cursor from parent
-                    outline: "none",   // Remove default button outline
-                }}
-                disabled={disabled}
-            >
-                {resolveIcon(
-                    startIcon,
-                    startIconPaths,
-                    startIconSize ?? iconSize,
-                    contentColor
-                )}
-                <span style={textStyle}>{title}</span>
-                {resolveIcon(
-                    endIcon,
-                    endIconPaths,
-                    endIconSize ?? iconSize,
-                    contentColor
-                )}
-            </button>
-        </Touchable>
-    );
-};
+            ...style,
+        };
 
+        const textStyle: React.CSSProperties = {
+            fontSize: sizeStyles[size]?.fontSize,
+            margin: "0 8px",
+            color: contentColor,
+            position: "relative",
+            top: -1,
+            ...titleStyle,
+        };
+
+        return (
+            <Touchable className={className} onClick={onClick} style={buttonStyle}>
+                <button
+                    onClick={(e) => e.preventDefault()}
+                    onMouseDown={handlePressStart}
+                    onMouseUp={handlePressEnd}
+                    onMouseLeave={handlePressEnd}
+                    onTouchStart={handlePressStart}
+                    onTouchEnd={handlePressEnd}
+                    style={{
+                        border: "none",
+                        backgroundColor: "transparent",
+                        width: "100%",
+                        height: "100%",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        cursor: "inherit",
+                        outline: "none",
+                        padding: 0
+                    }}
+                    disabled={disabled}
+                >
+                    {resolveIcon(
+                        startIcon,
+                        startIconPaths,
+                        startIconSize ?? iconSize,
+                        startIconColor ?? contentColor,
+                        startIconStyle
+                    )}
+                    {animateTitle ? (
+                        <TypingEffect
+                            text={title}
+                            speed={animationSpeed} // Usar la velocidad definida
+                            style={textStyle}
+                            showCursor={true}
+                            onComplete={handleAnimationComplete}
+                        />
+                    ) : (
+                        <span style={textStyle}>{title}</span>
+                    )}
+                    {showEndIcon &&
+                        resolveIcon(
+                            endIcon,
+                            endIconPaths,
+                            endIconSize ?? iconSize,
+                            endIconColor ?? contentColor,
+                            endIconStyle
+                        )}
+                </button>
+            </Touchable>
+        );
+
+    }
+)
 export default Button;
